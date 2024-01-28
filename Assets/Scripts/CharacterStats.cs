8aa38080b3d2db056f406f8b767af5d03e46bd01
chourouk07 <117457141+chourouk07@users.noreply.github.com>
2024-01-28 09:21:11 +0100

    Health+UltUI

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -10,12 +10,29 @@ public class CharacterStats : MonoBehaviour
     [SerializeField] Animator _animator;
     [SerializeField] GameObject _vfxHit;
     [SerializeField] GameObject _vfxDead;
+    [SerializeField] GameObject _vfxUltimate;
+    [SerializeField] Slider _ultSlider;
+    public float fillSpeed = 0.05f;
 
     [SerializeField] private float currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
+    }
+    private void Update()
+    {
+        if (_ultSlider != null)
+        {
+            if (_ultSlider.value == 1)
+            {
+                _ultSlider.value = 0;
+            }
+            else
+            {
+                FillUltOverTime();
+            }
+        }
     }
     public void ChangeHealth(float value)
     {
@@ -68,24 +85,17 @@ public class CharacterStats : MonoBehaviour
             Instantiate(_vfxDead, transform.position, Quaternion.identity);
         }
     }
+    private void FillUltOverTime()
+    {
+        // Increment the slider value based on time and fill speed
+        _ultSlider.value += fillSpeed * Time.deltaTime;
 
+        // Ensure that the value stays between 0 and 1
+        _ultSlider.value = Mathf.Clamp01(_ultSlider.value);
+    }
     public void OnDead()
     {
         Destroy(gameObject,0.1f);
     }
 
-    public float GetPower()
-    {
-        return power;
-    }
-    public void SetMaxHealth(float value)
-    {
-        maxHealth += value;
-        currentHealth = maxHealth;
-        if (healthSlider != null)
-        {
-            float fillAmount = currentHealth / maxHealth;
-            healthSlider.value = fillAmount;
-        }
-    }
 }
